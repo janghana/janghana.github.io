920652c434e92fb58abd485694b2b6efc7518951
janghana <han001112@icloud.com>
2025-01-03 15:41:03 -0800

    Updates

---
+++ precache-manifest.afd3900ad8833a665d381c3fd716c5bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36509561b6f1e8024227991f267ef94c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "116bc9790307bd5377b4",
+    "url": "/static/css/main.5e357cab.chunk.css"
+  },
+  {
+    "revision": "7cf550312aa855a7ffc7",
+    "url": "/static/js/2.ee53166b.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "116bc9790307bd5377b4",
+    "url": "/static/js/main.ceb6f0a9.chunk.js"
+  },
+  {
+    "revision": "38b441cba92b9c74f03f",
+    "url": "/static/js/runtime~main.16e34d5d.js"
+  },
+  {
+    "revision": "f5dbd01f9f5bad9a0152548d6e8828b1",
+    "url": "/static/media/han.f5dbd01f.jpeg"
+  }
+]);
